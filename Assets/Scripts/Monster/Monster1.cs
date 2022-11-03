9e1e9794aa5818813d78649150a232d07bfba737
SWC <st1020575@gmail.com>
2022-11-04 01:28:07 +0800

    Add monster2: Arrow, bow animation.

--- Assets/Scripts/Monster/Monster1.cs
+++ Assets/Scripts/Monster/Monster1.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 using Monster;
 
 public class Monster1 : Monster.Monster {
-    
-	private float atkAnimationTime;
-	private bool isPlayerGetDamage;
-	void Start() {
-		this.defaultStart();
-		this.isPlayerGetDamage = false;
-		this.HP = 100;
-	}
 
-	// Update is called once per frame
-	void Update() {
-		this.stateUpdate();
-		
-		if (this.isAtk) {
-			this.atkAnimationTime = this.mAnimatorState.normalizedTime % this.mAnimatorState.length;
-			this.isAtkToPlayer = this.atkAnimationTime > 0.7 && this.atkAnimationTime < 1.0;
-			if (this.atkAnimationTime < 0.7)
-				this.isPlayerGetDamage = false;
-		}
-	}
-	
-	void OnTriggerStay(Collider other) {
-		if (other.tag == "player" && this.isAtkToPlayer && !this.isPlayerGetDamage) {
-			this.isPlayerGetDamage = true;
-			// Player get damage
-		}
-	}
+    private float atkAnimationTime;
+    private bool isPlayerGetDamage;
+    void Start() {
+        this.defaultStart();
+        this.isPlayerGetDamage = false;
+        this.HP = 100;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        this.stateUpdate();
+
+        if (this.isAtk) {
+            this.atkAnimationTime = this.mAnimatorState.normalizedTime % 1;
+            this.isAtkToPlayer = this.atkAnimationTime > 0.5 && this.atkAnimationTime < 0.625;
+            if (this.atkAnimationTime < 0.5)
+                this.isPlayerGetDamage = false;
+        }
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (other.tag == "player" && this.isAtkToPlayer && !this.isPlayerGetDamage) {
+            this.isPlayerGetDamage = true;
+            // Player get damage
+        }
+    }
 }
--- Assets/Scripts/Monster/Monster2.cs
+++ Assets/Scripts/Monster/Monster2.cs
@@ -3,22 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using Monster;
 public class Monster2 : Monster.Monster {
-	private Vector3 atkAngleFix;
-	private bool isAtkAngleFix;
-	void Start() {
-		this.defaultStart();
-		this.HP = 100;
-		this.atkAngleFix = new Vector3(0f, 130f, 0f);
-		this.isAtkAngleFix = false;
-	}
+    public Animator bowAnimator;
+    public GameObject holdingArrow;
+    private float atkAnimationTime;
+    void Start() {
+        this.defaultStart();
+        this.HP = 100;
 
-	// Update is called once per frame
-	void Update() {
-		this.stateUpdate();
-		if (this.isAtk && !this.isAtkAngleFix) {
-			this.isAtkAngleFix = true;
-            this.gameObject.transform.Rotate(this.atkAngleFix);
-		}
+    }
 
-	}
+    // Update is called once per frame
+    void Update() {
+        this.stateUpdate();
+        if (!this.holdingArrow.activeSelf && this.isAtk)
+            this.holdingArrow.SetActive(true);
+        else if (this.holdingArrow.activeSelf && !this.isAtk)
+            this.holdingArrow.SetActive(false);
+        if (this.isAtk) {
+            this.atkAnimationTime = this.mAnimatorState.normalizedTime % 1;
+            this.bowAnimator.SetBool("isAtk", this.atkAnimationTime > 0.64);
+            Debug.Log(this.atkAnimationTime);
+        } else
+            this.bowAnimator.SetBool("isAtk", false);
+    }
 }
